1cd3e700f5e9a4d05746e2b9b50db8bb8156e72f
g33k1e <jquerolmar@gmail.com>
2022-12-04 23:46:54 +0100

    todos app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Item } from './models/item';
+import { TodolistService } from './services/todolist.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,76 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'to-do-app';
+  title = 'todo';
+
+
+
+  editActive:boolean = false;
+  id:string;
+  description:string;
+  allItems:Item[] = [];
+
+  constructor(private todolist:TodolistService){
+    this.id = '';
+    this.description = '';
+  }
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.getItems();
+  }
+
+  getItems():void{
+    this.todolist.getTodoList().subscribe((result:Item[])=>this.allItems = result);
+  }
+
+
+  addItem(description: string) {
+
+    if(description === ''){
+      return;
+    }
+
+
+
+
+
+    this.todolist.addTodo(description).subscribe(result=>{
+      if(result){
+        this.getItems();
+      }
+    })
+
+  }
+
+  editTodo(index:number):void{
+
+    this.editActive = true;
+    this.id = this.allItems[index]._id;
+    this.description = this.allItems[index].description;
+
+
+  }
+
+  sendEditTodo():void{
+
+    this.todolist.editTodo(this.id,this.description).subscribe(result=>{
+      if(result){
+        this.getItems();
+      }
+    })
+
+  }
+
+  deleteItem(index:number):void{
+
+
+    this.todolist.deleteTodo(this.allItems[index]._id).subscribe(result=>{
+      if(result){
+        this.getItems();
+      }
+    })
+
+  }
 }
